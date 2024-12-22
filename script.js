40b88108b843a8a67d10c9aa2e2f221564fc5a96
marquits <markozzel20@gmail.com>
2024-12-21 23:20:57 -0300

    Update script.js
    modificación 21/12/24

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-var nombre = "Marcos "; // tipo de variable
-let apellido = "Barber "; // tipo de variable
-console.log(apellido);
-alert("Bienvenido a " + nombre + apellido);
-var look = prompt("¿Estas en buscas de un buen look?");
+const boton = document.getElementById("botoncito");
 
-if (look == "si") {
-  // if = si para preguntar
-  alert("Perfecto!");
-} else {
-  // else = si es falso
-  alert("Lo siento..");
-  // Bloquear el acceso a la página
-  document.body.innerHTML =
-    "<h1>ERROR DE SITIO</h1><p>Lo sentimos, debes responder afirmativamente para acceder a esta página.</p>";
-  // Opcionalmente, puedes ocultar el contenido original
-  document.body.style.backgroundColor = "red"; // Cambia el color de fondo si lo deseas
-}
+boton.onclick = (e) => {
+  e.preventDefault(); //PARA EVITAR QUE EL FORMULARIO SE ENVIE
+
+  const nombre_apellido = document
+    .getElementById("nombre_apellido")
+    .value.trim();
+  const contacto = document.getElementById("contacto").value.trim();
+  const fecha = document.getElementById("fecha").value;
+  const hora = document.getElementById("hora").value;
+
+  if (nombre_apellido && contacto && fecha && hora) {
+    document.getElementById("nombre_apellido").value = "";
+    document.getElementById("contacto").value = "";
+    document.getElementById("fecha").value = "";
+    document.getElementById("hora").value = "";
+    alert("Solicitud de Turno Enviada");
+  } else {
+    alert("Falta Completar");
+  }
+};
+
+//EJEMPLOS DE LAS CLASES
+// var nombre = "Marcos "; // tipo de variable
+// let apellido = "Barber "; // tipo de variable
+// console.log(apellido);
+// //if (nombre && apellido)
+// alert("Solicitud de Turno enviada");
+
+// var look = prompt("¿Estas en buscas de un buen look? ¿Si? o ¿No?");
+// if ((look = Si)) {
+//   // if = Si para preguntar
+//   alert("Perfecto, este es el lugar correcto!");
+// } else {
+//   // else = si es falso
+//   alert("Espero que pronto cambies de opinión. Adíos");
+//   // Bloquear el acceso a la página
+//   document.body.innerHTML =
+//     "<h1>ERROR DE SITIO</h1><p>Lo sentimos, debes responder afirmativamente para acceder a esta página.</p>";
+//   // Opcionalmente, oculta el contenido original
+//   document.body.style.backgroundColor = "red"; // Color de fondo
+// }
